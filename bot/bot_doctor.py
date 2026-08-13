8d7d03685cccfd0e707717367e81b29fab165b2d
juanjo <juank480@gmail.com>
2026-08-13 13:55:10 +0200

    Fix issue with doctor-bot's docker containner

--- bot/bot_doctor.py
+++ bot/bot_doctor.py
@@ -1,6 +1,6 @@
 import os
 import logging
-from telegram import Update
+from telegram import Update, ReplyKeyboardMarkup
 from telegram.ext import (
     Application,
     ApplicationBuilder,
@@ -8,7 +8,6 @@ from telegram.ext import (
     ContextTypes,
     MessageHandler,
     filters,
-    ReplyKeyboardMarkup,
 )
 from database import db
 
